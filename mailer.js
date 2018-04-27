6f21f2826709fc8cbdfcd6d54ea40d32524a9b9a
codeRISEIO <33420264+coderiseio@users.noreply.github.com>
2018-04-27 02:07:30 -0400

    Create mailer.js

---
+++ mailer.js
@@ -0,0 +1,34 @@
+var nodemailer = require("nodemailer");
+
+var buildDisplayName = process.env.BUILD_DISPLAY_NAME
+var gitBranch        = process.env.GIT_BRANCH
+var gitCommit        = process.env.GIT_COMMIT
+var pipelineId       = process.env.PIPELINE_ID
+var recipientAddress = process.env.RECIPIENT_ADDRESS
+var projectName      = process.env.PROJECT_NAME
+var ibmUrl           = "https://console.bluemix.net/devops/pipelines/"
+
+console.log(process.env)
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("smtps://" + process.env.EMAIL_ID + ":"+encodeURIComponent('" + process.env.EMAIL_PWD + "') + "@smtp.gmail.com:465");
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "CI Builds <" + process.env.EMAIL_ID + ">", // sender address
+    to: recipientAddress, // list of receivers
+    subject: "[" + projectName + "] BUILD " + buildDisplayName + "(" + gitBranch + "-" + gitCommit + ")", // Subject line
+    html: "<b>Project:" + projectName + "</b><br /><br /><b>Build " + buildDisplayName + ": PASSED</b><br /><br />LINK: " + ibmUrl + pipelineId + "?env_id=ibm:yp:us-south<br /><br />" // html body
+}
+
+// send mail with defined transport object
+smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    smtpTransport.close(); // shut down the connection pool, no more messages
+});
