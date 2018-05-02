6997e9d5dd2f670db9b538f570322a3f92b86ae2
codeRISEIO <33420264+coderiseio@users.noreply.github.com>
2018-05-02 00:50:40 -0400

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -3,6 +3,7 @@ var nodemailer = require("nodemailer");
 var buildDisplayName = process.env.BUILD_DISPLAY_NAME
 var gitBranch        = process.env.GIT_BRANCH
 var gitCommit        = process.env.GIT_COMMIT
+var gitUrl           = process.env.GIT_URLS
 var pipelineId       = process.env.PIPELINE_ID
 var senderAddress    = "equibit.builds@gmail.com"
 var recipientAddress = process.env.RECIPIENT_ADDRESS
@@ -20,7 +21,7 @@ var mailOptions = {
     from: senderAddress, // sender address
     to: recipientAddress, // list of receivers
     subject: "[ Build " + buildStatus  + "] on " + projectName + " " + buildDisplayName + " (" + gitBranch + ")", // Subject line
-    html: "<b>Project: </b>" + projectName + "<br /><br /><b>Build: </b>" + buildDisplayName + "<br /><br /><b>Status: </b>" + buildStatus + "<br /><br /><b>Branch: </b>" + gitBranch + "<br /><br /><b>Commit: </b>"+ gitCommit +"<br/><br /><br /><br /><b>For more detailed information, use this link:</b>" + ibmUrl + pipelineId + "?env_id=ibm:yp:us-south<br /><br /><br />" // html body
+    html: "<b>Project: </b>" + projectName + "<br /><br /><b>Build: </b>" + buildDisplayName + "<br /><br /><b>Status: </b>" + buildStatus + "<br /><br /><b>Branch: </b>" + gitBranch + "<br /><br /><b>Commit: </b>"+ gitCommit +"<br/><br /><b>Repository: </b>"+ gitUrl +"<br/><br /><br /><b>For more detailed information, please use this link: </b>" + ibmUrl + pipelineId + "?env_id=ibm:yp:us-south<br /><br /><br />" // html body
 }
 
 // send mail with defined transport object
