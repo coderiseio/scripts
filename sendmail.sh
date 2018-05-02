051e6090909f8c1958f7ea2854c7f6d580b91a05
codeRISEIO <33420264+coderiseio@users.noreply.github.com>
2018-05-02 00:43:51 -0400

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
+export PROJECT_NAME="${CF_APP}"
 
 echo "BUILD STATUS: $BUILD_STATUS"
 echo "Sending email notifications to: $RECIPIENT_ADDRESS"
