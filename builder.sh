232b8d40e0adaa7318f26aef8608f084cf4ff3d0
codeRISEIO <33420264+coderiseio@users.noreply.github.com>
2018-05-04 09:15:08 -0400

    Create builder.sh

---
+++ builder.sh
@@ -0,0 +1,16 @@
+set -ex
+
+DEPLOY_RESULT="Success"
+BUILD_RESULT=$(cat result.txt)
+echo "$BUILD_RESULT"
+
+if [ "$BUILD_RESULT" == "Success" ]
+then
+  cf push "${CF_APP}" -c "NODE_ENV=$BUILD_COMMAND" || {  DEPLOY_RESULT="Failed"; }
+fi
+
+if [ "$BUILD_RESULT" == "Success" ]
+then
+  export "BUILD_STATUS=$DEPLOY_RESULT"
+  cd scripts && ./deploy-sendmail.sh
+fi
