a5d19a06a60304dc236924b779488f3f1d1cfa62
codeRISEIO <33420264+coderiseio@users.noreply.github.com>
2018-05-04 02:01:04 -0400

    Create deploy-sendmail.sh

---
+++ deploy-sendmail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PATH=/opt/IBM/node-v6.7.0/bin:$PATH
+export PROJECT_NAME="${CF_APP}"
+
+echo "BUILD STATUS: $BUILD_STATUS"
+echo "Sending email notification to: $RECIPIENT_ADDRESS"
+npm install nodemailer
+node deploy-mailer.js
